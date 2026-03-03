96d8957f49ac42b4f5e8caa1f380e10ceeeeb35a
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2026-03-03 11:14:37 -0700

    More structures

--- src/Actividad-10/index.php
+++ src/Actividad-10/index.php
@@ -1,6 +1,22 @@
 <?php
+echo "Actividad 10: Ciclo While<br><br>";
 $i = 0;
 while ($i < 10) {
   echo "Número: $i<br>";
   $i++;
 }
+echo "<br>¡Ciclo While terminado!";
+
+echo "<br><br>Actividad 10: Ciclo Do-While<br><br>";
+$j = 0;
+do {
+  echo "Número: $j<br>";
+  $j++;
+} while ($j < 10);
+echo "<br>¡Ciclo Do-While terminado!";
+
+echo "<br><br>Actividad 10: Ciclo For<br><br>";
+for ($k = 0; $k < 10; $k++) {
+  echo "Número: $k<br>";
+}
+echo "<br>¡Ciclo For terminado!";
